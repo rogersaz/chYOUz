e712727349e3935b4205e2edf7b7318219671bc3
Mark <45799393+rogersaz@users.noreply.github.com>
2024-06-25 13:36:11 -0700

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -17,11 +17,12 @@ export default function Index() {
               <div className="absolute inset-0 bg-[color:rgba(139,92,246,0.5)] mix-blend-multiply" />
             </div>
             <div className="lg:pb-18 relative px-4 pt-16 pb-8 sm:px-6 sm:pt-24 sm:pb-14 lg:px-8 lg:pt-32">
-              <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
-                <span className="block uppercase text-orange-500 drop-shadow-md">
-                  chYOUz
-                </span>
-              </h1>
+            <h1 className="font-savoye text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
+            <span className="block uppercase text-orange-500 drop-shadow-md">
+              chYOUz
+            </span>
+            </h1>
+
               <p className="mx-auto mt-6 max-w-lg text-center text-xl text-white sm:max-w-3xl">
                 Personalized songs for your moments and memories.
               </p>
