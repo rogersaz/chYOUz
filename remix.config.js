0b01fc56f4cb0456f4b7e9508004ba3f451b52e5
Mark <45799393+rogersaz@users.noreply.github.com>
2024-06-26 09:28:13 -0700

    Update remix.config.js

--- remix.config.js
+++ remix.config.js
@@ -1,19 +1,11 @@
-// remix.config.js
-import { config as netlifyConfig } from "@netlify/remix-adapter";
-
-/** @type {import('@remix-run/dev').AppConfig} */
-export default {
-  ...(process.env.NODE_ENV === "production" ? netlifyConfig : undefined),
-  future: {
-    v2_meta: true, // Enable future Remix features if needed
-  },
-  webpack: (config) => {
-    // Add a loader for .html files
-    config.module.rules.push({
+module.exports = {
+  // ... other Remix configuration
+  loaders: [
+    {
       test: /\.html$/,
-      use: 'html-loader',
-    });
-    return config;
-  },
+      type: "asset/source",
+    },
+  ],
 };
+
 
